4b4703897d4eb5cdcc8a850e0194f118cac6fc1e
sornashanthi <sornashanthi.d@rajalakshmi.edu.in>
2024-03-02 19:02:03 +0530

    First Commit

---
+++ set1.py
@@ -0,0 +1,7 @@
+a=set()
+a=(1,2,3,4)
+print(type(a))
+a=list(a)
+x=tuple(a)
+for i in range(0,3):
+    print(a[i])
---
+++ sum1.py
@@ -0,0 +1,6 @@
+str1=input()#1234
+l2=map(int,str1)
+for i in l2:
+    print(i)
+    
+    
